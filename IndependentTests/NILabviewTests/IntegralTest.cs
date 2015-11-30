1f7cbe9b35b273ff5a3e3f786363c6c2417c0971
Thad House <thadhouse1@gmail.com>
2015-11-29 22:22:40 -0800

    Makes derivative  and integral tests use a set Dt
    Unit tests are for just testing the base front end. On a real robot the
    Dt will be dynamic, so waiting an exact time isnt important. So we can
    force otherwise.

--- IndependentTests/NILabviewTests/IntegralTest.cs
+++ IndependentTests/NILabviewTests/IntegralTest.cs
@@ -13,7 +13,8 @@ namespace IndependentTests.NILabviewTests
         public void PositiveIntegralTest1Step()
         {
             Integral t = new Integral();
-            Utility.AccurateWaitSeconds(1);
+            //Utility.AccurateWaitSeconds(1);
+            t.Dt = 1.0;
             Assert.AreEqual(1, t.Get(1), 0.02);
         }
 
@@ -21,9 +22,10 @@ namespace IndependentTests.NILabviewTests
         public void PositiveIntegralTest10Step()
         {
             Integral t = new Integral();
+            t.Dt = 0.1;
             for(int i = 0; i < 10; i++)
             {
-                Utility.AccurateWaitMilliseconds(100);
+                //Utility.AccurateWaitMilliseconds(100);
                 Assert.AreEqual(0.1 * (i + 1), t.Get(1), 0.02);
             }
         }
@@ -32,7 +34,8 @@ namespace IndependentTests.NILabviewTests
         public void NegativeIntegralTest1Step()
         {
             Integral t = new Integral();
-            Utility.AccurateWaitSeconds(1);
+            t.Dt = 1;
+            //Utility.AccurateWaitSeconds(1);
             Assert.AreEqual(-1, t.Get(-1), 0.02);
         }
 
@@ -40,9 +43,10 @@ namespace IndependentTests.NILabviewTests
         public void NegativeIntegralTest10Step()
         {
             Integral t = new Integral();
+            t.Dt = 0.1;
             for (int i = 0; i < 10; i++)
             {
-                Utility.AccurateWaitMilliseconds(100);
+                //Utility.AccurateWaitMilliseconds(100);
                 Assert.AreEqual(-0.1 * (i + 1), t.Get(-1), 0.02);
             }
         }
