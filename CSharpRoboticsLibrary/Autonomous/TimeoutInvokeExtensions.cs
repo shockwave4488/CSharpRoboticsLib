ddd6e106252e974296a2c240888ce35b08bdd95c
lessthantrue <nickm@outofthisworld.net>
2015-12-06 16:18:31 -0800

    New-ish method of doing autonomous
    Same underlying method as before (methods with time-outs) but just a
    different, hopefully less awful way of doing it.
    
    Also added a project for test cases requiring the WPILib simulator.

---
+++ CSharpRoboticsLibrary/Autonomous/TimeoutInvokeExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharpRoboticsLib.Autonomous
+{
+    public static class TimeoutInvokeExtensions
+    {
+        public static bool TryExecute<T>(this Func<T> f, int timeoutMs, out T result)
+        {
+            T t = default(T);
+            Thread thread = new Thread(() => t = f());
+            thread.Start();
+
+            bool completed = thread.Join(timeoutMs);
+            if (!completed)
+                thread.Abort();
+
+            result = t;
+            return completed;
+        }
+
+        public static T TryExecute<T>(this Func<T> f, int timeoutMs)
+        {
+            T result;
+            f.TryExecute(timeoutMs, out result);
+            return result;
+        }
+
+        public static bool TryExecute(this Action a, int timeoutMs)
+        {
+            Thread thread = new Thread(() => a());
+            thread.Start();
+
+            bool completed = thread.Join(timeoutMs);
+            if (!completed)
+                thread.Abort();
+
+            return completed;
+        }
+    }
+}
---
+++ IndependentTests/AutonomousTests/TimeoutInvokeTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using CSharpRoboticsLib.Autonomous;
+using NUnit.Framework;
+
+namespace IndependentTests.AutonomousTests
+{
+    [TestFixture]
+    public class SafeActionTests
+    {
+        private bool m_basicMethodRun;
+
+        [Test]
+        public void TimeoutInvokeTest()
+        {
+            Action a = TimeoutInvokeHelper;
+            Assert.IsTrue(a.TryExecute(20));
+            Assert.IsTrue(m_basicMethodRun);
+        }
+
+        private void TimeoutInvokeHelper()
+        {
+            m_basicMethodRun = true;
+        }
+
+        [Test]
+        public void TimeoutInvokeTimeoutTest()
+        {
+            Action a = TimeoutInvokeTimeoutHelper;
+            Assert.IsFalse(a.TryExecute(10));
+        }
+
+        private void TimeoutInvokeTimeoutHelper()
+        {
+            SpinWait.SpinUntil(() => false);
+        }
+
+        [Test]
+        public void TimeoutInvokeReturnTest()
+        {
+            Func<int> f = TimeoutInvokeReturnHelper;
+            int i;
+            Assert.IsTrue(f.TryExecute(10, out i));
+            Assert.AreEqual(i, 1);
+
+            Assert.AreEqual(f.TryExecute(10), 1);
+        }
+
+        public int TimeoutInvokeReturnHelper()
+        {
+            return 1;
+        }
+    }
+}
