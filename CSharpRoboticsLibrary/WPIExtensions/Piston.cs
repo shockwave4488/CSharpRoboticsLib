84ba9e9b6cebe28bb81fd8ffdb43adc6467611d8
lessthantrue <nickm@outofthisworld.net>
2015-12-27 21:58:17 -0800

    Added Piston class, slight bit of organizational stuff
    Renamed Utility -> Util because the class and namespace names were
    interfering

---
+++ CSharpRoboticsLibrary/WPIExtensions/Piston.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPILib;
+
+namespace CSharpRoboticsLib.WPIExtensions
+{
+    /// <summary>
+    /// A subclass of DoubleSolenoid meant to more intuitively model a piston
+    /// </summary>
+    public class Piston : DoubleSolenoid
+    {
+        private bool m_extended;
+        private bool m_inverted;
+
+        /// <summary>
+        /// True if the piston is extended, false if not
+        /// </summary>
+        public bool Extended
+        {
+            get { return m_extended; }
+            set
+            {
+                m_extended = value;
+                value ^= m_inverted;
+                Set(value ? Value.Forward : Value.Reverse);
+            }
+        }
+
+        /// <summary>
+        /// Inverts the setting of the piston
+        /// </summary>
+        public bool Inverted
+        {
+            get { return m_inverted; }
+            set
+            {
+                m_inverted = value;
+                Extended = Extended; //Update Extended
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="forwardChannel"></param>
+        /// <param name="reverseChannel"></param>
+        public Piston(int forwardChannel, int reverseChannel) : base(forwardChannel, reverseChannel)
+        {
+            Extended = Inverted = false;
+        }
+
+        public Piston(int moduleNumber, int forwardChannel, int reverseChannel) : base(moduleNumber, forwardChannel, reverseChannel)
+        {
+            Extended = Inverted = false;
+        }
+    }
+}
---
+++ SimulatorTests/ExtensionsTests/PistonTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CSharpRoboticsLib.WPIExtensions;
+using HAL.Simulator;
+
+namespace SimulatorTests.ExtensionsTests
+{
+    [TestFixture]
+    public class PistonTests : SimTestBase
+    {
+        [Test]
+        public static void PistonBasicTest()
+        {
+            using (Piston p = new Piston(0, 1))
+            {
+                Assert.IsFalse(SimData.GetPCM(0).Solenoids[0].Value);
+                Assert.IsTrue(SimData.GetPCM(0).Solenoids[1].Value);
+                p.Extended = true;
+                Assert.IsTrue(SimData.GetPCM(0).Solenoids[0].Value);
+                Assert.IsFalse(SimData.GetPCM(0).Solenoids[1].Value);
+                p.Extended = false;
+                Assert.IsFalse(SimData.GetPCM(0).Solenoids[0].Value);
+                Assert.IsTrue(SimData.GetPCM(0).Solenoids[1].Value);
+            }
+        }
+
+        [Test]
+        public static void PistonInvertedTest()
+        {
+            using (Piston p = new Piston(0, 1) {Inverted = true})
+            {
+                Assert.IsTrue(SimData.GetPCM(0).Solenoids[0].Value);
+                Assert.IsFalse(SimData.GetPCM(0).Solenoids[1].Value);
+                p.Extended = true;
+                Assert.IsFalse(SimData.GetPCM(0).Solenoids[0].Value);
+                Assert.IsTrue(SimData.GetPCM(0).Solenoids[1].Value);
+                p.Extended = false;
+                Assert.IsTrue(SimData.GetPCM(0).Solenoids[0].Value);
+                Assert.IsFalse(SimData.GetPCM(0).Solenoids[1].Value);
+            }
+        }
+    }
+}
