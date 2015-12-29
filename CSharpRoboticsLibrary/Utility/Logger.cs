ad81d387164aa1532e985e2e8b70822d391631bc
lessthantrue <nickm@outofthisworld.net>
2015-12-28 21:05:50 -0800

    Added a message logger, having trouble with tests
    mostly just the SmartDashboard test. I apparently need to initialize
    that somehow. I wouldn't think I would need to use the simulator for
    this.

---
+++ CSharpRoboticsLibrary/Utility/Logger.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WPILib.SmartDashboard;
+
+namespace CSharpRoboticsLib.Utility
+{
+    /// <summary>
+    /// Static class that can log all information to a text file or SmartDashboard variable
+    /// </summary>
+    public static class Logger
+    {
+        private struct TimeStampedMessage
+        {
+            private TimeSpan m_timestamp;
+            private string m_message;
+
+            public TimeStampedMessage(string message)
+            {
+                m_message = message;
+                m_timestamp = DateTime.Now - _offset;
+            }
+
+            public override string ToString()
+            {
+                return $"[ {m_timestamp.Minutes} : {m_timestamp.Seconds}.{m_timestamp.Milliseconds} ]\t" + m_message;
+            }
+        }
+
+        private static DateTime _offset;
+        private static List<TimeStampedMessage> _messages;
+
+        static Logger()
+        {
+            SmartDashboardVariable = null;
+            _offset = DateTime.Now;
+            _messages = new List<TimeStampedMessage>();
+            AddMessage("Logger Initialized");
+        }
+
+        /// <summary>
+        /// <see cref="SmartDashboard"/> variable name to automatically write to
+        /// </summary>
+        public static string SmartDashboardVariable { get; set; }
+
+        /// <summary>
+        /// Resets the time value to zero
+        /// </summary>
+        public static void ResetTimer()
+        {
+            AddMessage("Timer Reset");
+            _offset = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Adds a message at the current time
+        /// </summary>s
+        /// <param name="message"></param>
+        public static void AddMessage(string message)
+        {
+            _messages.Add(new TimeStampedMessage(message));
+            UpdateSmartDashboard();
+        }
+
+        /// <summary>
+        /// Returns the contents of the <see cref="Logger"/>
+        /// </summary>
+        /// <returns></returns>
+        public new static string ToString()
+        {
+            string toReturn = "";
+            foreach (TimeStampedMessage t in _messages)
+            {
+                toReturn += t + "\n";
+            }
+            return toReturn;
+        }
+
+        /// <summary>
+        /// Saves the log as a text file
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static void Save(string filePath)
+        {
+            //I have no idea if this will work on the roborio.
+            File.WriteAllText(filePath, ToString());
+        }
+
+        /// <summary>
+        /// Clears all messages
+        /// </summary>
+        public static void Clear()
+        {
+            _messages.Clear();
+        }
+
+        /// <summary>
+        /// Updates the related <see cref="SmartDashboard"/> variable if applicable
+        /// </summary>
+        public static void UpdateSmartDashboard()
+        {
+            if (null != SmartDashboardVariable)
+                SmartDashboard.PutString(SmartDashboardVariable, ToString());
+        }
+    }
+}
---
+++ IndependentTests/UtilityTests/LoggerTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPILib.SmartDashboard;
+using CSharpRoboticsLib.Utility;
+using NUnit.Framework;
+
+namespace IndependentTests.UtilityTests
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        [Test]
+        public static void AddMessageTest()
+        {
+            Logger.Clear();
+            Logger.AddMessage("Hello World!");
+            Assert.AreNotEqual("", Logger.ToString());
+
+            //For manual checking
+            string s = Logger.ToString();
+        }
+
+        [Test]
+        public static void SmartDashboardTest()
+        {
+            //I know I need to initialize the SmartDashboard somehow... not sure how though.
+            Logger.Clear();
+            string var = "TEST";
+            //Logger.SmartDashboardVariable = var;
+            Logger.AddMessage("Hello SmartDashboard!");
+
+            // I know its deprecated, but I'm not sure what to use in its place.
+            // SmartDashboard.GetData() returns an ISendable, which I don't know how to use.
+            //Assert.IsNotNull(SmartDashboard.GetString(var));
+            //Assert.AreNotEqual("", SmartDashboard.GetString(var));
+        }
+
+        [Test] //This is just going to be a manual test - me looking at values making sure it works.
+        public static void TimerResetTest()
+        {
+            Logger.Clear();
+            Logger.AddMessage("Before Reset");
+            Util.AccurateWaitMilliseconds(1000);
+            Logger.ResetTimer();
+            Logger.AddMessage("After Reset");
+
+            string s = Logger.ToString();
+        }
+    }
+}
