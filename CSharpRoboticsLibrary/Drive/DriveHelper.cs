6ac8e8817c988b8186fe43e7d5179ad2472d0e12
lessthantrue <nickm@outofthisworld.net>
2016-01-18 11:45:19 -0800

    Realized I screwed up the namespace of this one
    oops

--- CSharpRoboticsLibrary/Drive/DriveHelper.cs
+++ CSharpRoboticsLibrary/Drive/DriveHelper.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CSharpRoboticsLib.Drive.Interfaces;
-using CSharpRoboticsLib.Utility;
 using WPILib;
 
-namespace _2015_Pre_build_week_project.Team_Code.Drive_Code
+namespace CSharpRoboticsLib.Drive
 {
     //Shamelessly copy-pasted from Team 254©'s Robot Code©
 
